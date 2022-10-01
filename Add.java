f204a55c24704c8852c7a3181ff047d6c1d4fd98
Manjunath <manjunathreddy14@gmail.com>
2022-10-01 11:23:43 +0530

    created ArrayList

--- Add.java
+++ Add.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class Add {
     public static void main(String[] args) {
         System.out.println("Add");
+        ArrayList al = new ArrayList();
     }
 
     public void methodRoopa(){
