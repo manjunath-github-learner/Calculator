a5f8c3de3401e9577eec85114dbb6ba230fa011e
Manjunath <manjunathreddy14@gmail.com>
2022-10-01 11:26:35 +0530

    created RRRRRRRRR

--- Add.java
+++ Add.java
@@ -12,7 +12,7 @@ public class Add {
 
     public void methodDuck(){
         System.out.println("methodDuck");
-        System.out.println("methodDuck");
+        System.out.println("methodDucRRRRRRRRRRRRRRR");
     }
 
     public void methodAnimal(){
